a4e9036adfac9ab8c775190d850b424f61a0b276
therealcodeelo <therealcodeelo@users.noreply.github.com>
2022-06-11 21:16:07 +0700

    Добавьте файлы проекта.

---
+++ CodeeloSQL.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+
+namespace DesktopCalendar
+{
+    class CodeeloSQL
+    {
+        public static readonly string CONNECTION_STRING = @"Data Source = " + Environment.CurrentDirectory
+            + @"\Calendar.db; Version=3;";
+        private static SQLiteConnection _connection;
+        private static SQLiteCommand _command;
+
+        public static List<Appointment> GetAppointments(DateTime date)
+        {
+            var appointments = new List<Appointment>();
+            using (_connection = new SQLiteConnection(CONNECTION_STRING))
+            {
+                _connection.Open();
+                _command = new SQLiteCommand();
+                _command.Connection = _connection;
+                _command.CommandText = $"Select * from Appointments where EndDate like '{date.ToString("yyyy-MM-dd")}'";
+                var reader = _command.ExecuteReader();
+                while(reader.Read())
+                {
+                    var appointment = new Appointment();
+
+                    appointment.ID = int.Parse(reader["ID"].ToString());
+                    appointment.Title = reader["Title"].ToString();
+                    appointment.Description = reader["Description"].ToString();
+                    appointment.EndDate = Convert.ToDateTime(reader["EndDate"].ToString());
+                    appointment.IsCompleted = Convert.ToBoolean(reader["IsCompleted"]);
+                    appointment.Result = reader["Result"].ToString();
+                    appointments.Add(appointment);
+                }
+            }
+            return appointments;
+        }
+
+        public static void AddAppointment(Appointment appointment)
+        {
+            using (_connection = new SQLiteConnection(CONNECTION_STRING))
+            {
+                _connection.Open();
+                _command = new SQLiteCommand();
+                _command.Connection = _connection;
+                _command.CommandText = "Insert into Appointments(Title,Description,EndDate,IsCompleted)" +
+                    $"values('{appointment.Title}','{appointment.Description}','{appointment.EndDate.ToString("yyyy-MM-dd")}',0)";
+                _command.ExecuteNonQuery();
+            }
+        }
+        public static void UpdateAppointment(Appointment appointment)
+        {
+            using (_connection = new SQLiteConnection(CONNECTION_STRING))
+            {
+                _connection.Open();
+                _command = new SQLiteCommand();
+                _command.Connection = _connection;
+                if(appointment.IsCompleted)
+                {
+                    _command.CommandText = $"update Appointments set Title='{appointment.Title}',Description='{appointment.Description}'," +
+                        $"EndDate='{appointment.EndDate.ToString("yyyy-MM-dd")}',IsCompleted=1, Result = '{appointment.Result}' where ID={appointment.ID}";
+                }
+                else
+                {
+                    _command.CommandText = $"update Appointments set Title='{appointment.Title}',Description='{appointment.Description}'," +
+                        $"EndDate='{appointment.EndDate.ToString("yyyy-MM-dd")}' where ID={appointment.ID}";
+                }
+                
+                _command.ExecuteNonQuery();
+            }
+        }
+
+        public static void ExecuteQuery(string query)
+        {
+            using (_connection = new SQLiteConnection(CONNECTION_STRING))
+            {
+                _connection.Open();
+                _command = new SQLiteCommand();
+                _command.Connection = _connection;
+                _command.CommandText = query;
+                _command.ExecuteNonQuery();
+            }
+        }
+        public static int GetNextAppointmentID()
+        {
+            using (_connection = new SQLiteConnection(CONNECTION_STRING))
+            {
+                _connection.Open();
+                _command = new SQLiteCommand();
+                _command.Connection = _connection;
+                _command.CommandText = "select seq from sqlite_sequence where name like 'Appointments'";
+                return (Convert.ToInt32(_command.ExecuteScalar()) + 1);
+            }
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,26 @@
+﻿namespace DesktopCalendar
+{
+    internal class Singleton
+    {
+        private static Singleton _instance;
+        private MainForm _mainForm;
+        public static Singleton Instance
+        {
+            get 
+            { 
+                if(_instance == null)
+                    _instance = new Singleton();
+                return _instance; 
+            }
+        }
+        public  MainForm MainForm
+        {
+            get
+            {
+                if(_mainForm == null || _mainForm.IsDisposed)
+                    _mainForm = new MainForm();
+                return _mainForm;
+            }
+        }
+    }
+}
